f4205a96cb7463ecfd2256f002a3c1680c8fde84
byron <byronap120@gmail.com>
2018-09-14 21:27:19 -0600

    Tripe shoot power up

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    [SerializeField] private float _speed = 10f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        if(transform.position.y > 7.50)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,12 @@
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 1f;
+    public bool canTripleShoot = false;
+
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _fireRate = 0.25F;
+    private float _nextFire = 0.0F;
 
     // Use this for initialization
     void Start()
@@ -15,13 +19,17 @@ public class Player : MonoBehaviour
     void Update()
     {
         UpdatePlayerPosition();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
     }
 
     private void UpdatePlayerPosition()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime);
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
         VerifyPlayerPosition();
     }
 
@@ -44,5 +52,21 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11, transform.position.y, 0);
         }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _nextFire)
+        {
+            _nextFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.95f, 0), Quaternion.identity);
+
+            if (canTripleShoot)
+            {
+                Instantiate(_laserPrefab, transform.position + new Vector3(-0.6f, 0.3f, 0), Quaternion.identity);
+                Instantiate(_laserPrefab, transform.position + new Vector3(0.6f, 0.3f, 0), Quaternion.identity);
+            }
+        }
+
     }
 }
