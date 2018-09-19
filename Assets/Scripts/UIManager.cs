8226419d1710f031b929590cc26421c05b86e6b2
byron <byronap120@gmail.com>
2018-09-18 22:59:30 -0600

    UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public Sprite[] lives;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScore()
+    {
+
+    }
+
+    public void UpdateLives()
+    {
+
+    }
+}
